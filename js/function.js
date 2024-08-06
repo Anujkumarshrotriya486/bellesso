454559859633426ba05278264104cc8a8eec9077
ANUJKUMARSHROTRIYA <anuj.shrotriya@edcast.com>
2024-08-06 13:21:18 +0530

    update_qa2

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiMTRhYTM0MTY5MzFlYzdiZWJmYjIxMzZmZjQ1OGU3OTcifQ.z9et8GXlHRz8IqE5wre-xsJxZeTZIojrn3yC1hNPzYI";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiOTY1ZmVmZDZkNzI1OGU5ZTg5YTE2MjVhZGMyNjZhYmIifQ.ZvDcjxa3q9SM-AF_niVZWEIhIxi0c4peE8hktfQreKs";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
