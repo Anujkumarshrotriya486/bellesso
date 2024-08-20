51bd3d213f9787a4049c6116bb27ec1a1579a4d9
Raunak Singh <37996026+raunakEdCast@users.noreply.github.com>
2024-08-20 18:51:29 +0530

    Update JWT Token in function.js

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiMjA2ZjljZjcwZmEyNThiODE2YmI5ZjFiNmJiMzRiMmEifQ.--tAh6BILuyuc34LJSsKqVt9ydwJ975gLq1fr4a_kdU";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiNDk0MjBjNzM4NGM1ZWRiNTk1ZTlmNzQ1NmM1NzUxNDkifQ.w9spHjPyCQ2tVuO8iay1uh6ajAtg3Iv2HW0OtEFVNwY";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
