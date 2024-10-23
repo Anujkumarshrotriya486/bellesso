285412671c6fa29640bd93ba45ae5240371da5f6
Raunak Singh <37996026+raunakEdCast@users.noreply.github.com>
2024-10-23 23:19:11 +0530

    testing

--- js/function.js
+++ js/function.js
@@ -7,4 +7,4 @@
 
 // window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiYzU4MzllY2NiM2ZhNzVjN2EzYTA4ZDg5NDZmYmZmYWMifQ._WHDZWNJ3FVEH0F7mFT1NsD7T-ZSR0qbbV5bRxLkgbY";
 
-if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://stagecdn.guideme.io/guideme-auth-stage/playerjs1/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
+// if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://stagecdn.guideme.io/guideme-auth-stage/playerjs1/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
