d786b47ceeede78a8efb1ec5b0d7ae1cf4a7d65f
Raunak Singh <37996026+raunakEdCast@users.noreply.github.com>
2024-10-24 15:38:42 +0530

    testing

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-// window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiMTg2NDZmNGU3NjVkMGMzNjc5YzAxN2FjNzU5YjdlYWUifQ.DSB7MC2JBLKscU3pIzNghZI6_DJJY7hjWGB4FOUJXdk";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiMTg2NDZmNGU3NjVkMGMzNjc5YzAxN2FjNzU5YjdlYWUifQ.DSB7MC2JBLKscU3pIzNghZI6_DJJY7hjWGB4FOUJXdk";
 
-// if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
+if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
