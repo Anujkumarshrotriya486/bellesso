2836b30e74560aab84194ceb6786bc768c34df9b
edcastashutosh <ashutosh@edcast.com>
2024-11-05 11:48:32 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiMTg2NDZmNGU3NjVkMGMzNjc5YzAxN2FjNzU5YjdlYWUifQ.DSB7MC2JBLKscU3pIzNghZI6_DJJY7hjWGB4FOUJXdk";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiZmMyODc3NmU1ZGU4YWNlYzVmZjVlMzQ0YTdmNmQxN2YifQ.MJDGU82syZUttGw2JhShLVF3uljrsxrMvG0U2bXP-Lc";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
