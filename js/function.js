6161bf4c11056c050f80bed0ede6670f41efa37d
edcastashutosh <ashutosh@edcast.com>
2024-11-07 17:53:29 +0530

    testing

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiZmMyODc3NmU1ZGU4YWNlYzVmZjVlMzQ0YTdmNmQxN2YifQ.MJDGU82syZUttGw2JhShLVF3uljrsxrMvG0U2bXP-Lc";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiYTNmMzM2NjYzMDMzMzM3ZDY5MTc3YTM2ZTBhOGY4NjkifQ.1FkAgnZcCgMX4czz5WLw1_nFW3Q2MV6wz-UGW8vQDFw";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
--- player/guideme.js
+++ player/guideme.js
@@ -3,7 +3,7 @@ if (typeof guideMe === 'undefined') {
 	guideMe = {};
 }
 if (!guideMe.baseUrl) {
-	guideMe.baseUrl = "//qa2-cdn.guideme.io/guideme-player/ent/";
+	guideMe.baseUrl = "https://cdn.myguidepreview.org/guideme-player/ent/";
 }
 configPath = guideMe.baseUrl + 'config.js';
 var playerExtImgUrl = "";
@@ -43,9 +43,9 @@ function getScriptCB() {
 
 	function loadGuideMeClientFiles() {
 		if (window.self === window.top) {
-			a.src = GmCXt.conf.baseUrl + 'gm_client_1730792012078.js';
+			a.src = GmCXt.conf.baseUrl + 'gm_client_1730981612081.js';
 		} else {
-			a.src = GmCXt.conf.baseUrl + 'gm_client_iframe_1730792012078.js';
+			a.src = GmCXt.conf.baseUrl + 'gm_client_iframe_1730981612081.js';
 		}
 		document.head.appendChild(a);
 	}
