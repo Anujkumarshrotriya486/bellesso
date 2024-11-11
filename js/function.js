35875ef55afe2309233865a97f6eccdb3dbe8e8c
edcastashutosh <60600478+edcastashutosh@users.noreply.github.com>
2024-11-11 11:43:22 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiYTNmMzM2NjYzMDMzMzM3ZDY5MTc3YTM2ZTBhOGY4NjkifQ.-YKksPwYyNpByXLqwXMOO2ynoECA7vuVOZKzC9jYMrM";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiZmZjZmUzZDU1NzMwMDE0YWUwMTAzNGY2YzlmNzdlMDUifQ.EeUoVvDISjyzJtKK3fmSQT8g2iw1roMulfwgPVYw8Qc";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
