b5996adbbf93e4cdfe1d900cf580bce763bf9d1d
Uday Patne <Uday@edcast.com>
2024-12-06 11:06:49 +0530

    Removed Preview JS

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiY2NlNTA5ZDI2ZjY3ZDRiMGI1NWJlOTQxMDVlMTcwYTQifQ.68ARhkJNoQmvYqgtyakKGfoDPL4RnZ4a2HSpesNKzZ4";
+window.myGuideOrgKey = "";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
