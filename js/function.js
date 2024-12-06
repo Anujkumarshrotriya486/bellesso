dd089045b8ee2c393472ee171893446704220cd4
Uday Patne <Uday@edcast.com>
2024-12-06 22:31:37 +0530

    Prod Build

--- js/function.js
+++ js/function.js
@@ -5,6 +5,6 @@
 // 	});
 // });
 
-window.myGuideOrgKey = "";
+window.myGuideOrgKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfa2V5IjoiZDliOWQ1MDdlMDIxZTUxNDI3OTg2OGRiNjQ1NjBjYjMifQ.MRyRFyxslk6VMZVsis0MlSIzqXbcnBbtkKdh0tnBXaI";
 
 if(!window.guideMe)window.guideMe={}; window.guideMe.baseUrl='https://anujkumarshrotriya486.github.io/bellesso/player/';var a=document.createElement('script');a.src=guideMe.baseUrl+'guideme.js';document.head.appendChild(a);
